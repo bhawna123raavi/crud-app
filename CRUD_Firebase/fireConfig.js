b539066203c8e73384e6c07a8dfd3917b7570670
bhawna123raavi <158606528+bhawna123raavi@users.noreply.github.com>
2024-03-11 15:09:46 +0530

    Add files via upload

---
+++ CRUD_Firebase/fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+
+    apiKey: "AIzaSyAPiI5g2QbDeC_Wiir1VW43RGkuhMoMlIA",
+    authDomain: "addfire-c1dbe.firebaseapp.com",
+    projectId: "addfire-c1dbe",
+    storageBucket: "addfire-c1dbe.appspot.com",
+    messagingSenderId: "414039101903",
+    appId: "1:414039101903:web:62fd3eb574676dcd8b02ba"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
